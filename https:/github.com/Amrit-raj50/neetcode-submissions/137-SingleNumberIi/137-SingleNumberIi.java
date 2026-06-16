4cde7244d6c0fcaa6fffb3f11bd2f8c29160ff73
Amrit Raj <amrit.raj.cg@gmail.com>
2026-06-16 14:13:27 +0530

    Create 137-SingleNumberIi

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,17 @@
+// Last updated: 16/06/2026, 14:13:24
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i = 0 ; i < nums.length ; i++){
+            int count = 0;
+            for(int j = 0 ; j < nums.length ; j++){
+                if((nums[i] == nums[j]) && (i != j)){
+                    count++;
+                }
+            }
+            if(count == 0){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
