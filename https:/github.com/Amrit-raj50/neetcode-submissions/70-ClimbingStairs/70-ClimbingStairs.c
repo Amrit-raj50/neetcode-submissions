c2e04fb2ab8996b304c3d20874163b722c705e61
Amrit Raj <amrit.raj.cg@gmail.com>
2026-06-16 14:13:34 +0530

    Create 70-ClimbingStairs

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/70-ClimbingStairs/70-ClimbingStairs.c
@@ -0,0 +1,22 @@
+// Last updated: 16/06/2026, 14:13:31
+int climbStairs(int n) {
+    int no1 = 1; 
+    int no2 = 2;
+    int no3;
+    if(n == 0)
+    no3 = 0;
+    else if(n == 1)
+    no3 = 1;
+    else if(n == 2)
+    no3 = 2;
+    else{
+        for(int i = 1; i < n-1;i++)
+        {
+            no3 = no1 + no2;
+            no1 = no2;
+            no2= no3;
+        }
+    }
+    
+    return no3;
+}
