330a8a981a20bfda8cf30a1d3129ecd6fb6e5eba
Amrit Raj <amrit.raj.cg@gmail.com>
2026-06-16 14:12:48 +0530

    Create 657-RobotReturnToOrigin

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/657-RobotReturnToOrigin/657-RobotReturnToOrigin.cpp
@@ -0,0 +1,25 @@
+// Last updated: 16/06/2026, 14:12:45
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for(int i = 0 ; i < moves.size(); i++){
+            if(moves[i] == 'U'){
+                y+=1;
+            }
+            else if(moves[i] == 'D'){
+                y-=1;
+            }
+            else if(moves[i] == 'L'){
+                x-=1;
+            }
+            else{
+                x+=1;
+            }
+        }
+
+        return (x == 0 && y == 0);
+        
+    }
+};
