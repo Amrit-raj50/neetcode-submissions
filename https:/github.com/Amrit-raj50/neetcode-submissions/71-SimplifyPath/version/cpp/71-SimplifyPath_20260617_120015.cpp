3da7a50da91f9948dec3232a7a4dc7ca8f3ec72f
Amrit Raj <amrit.raj.cg@gmail.com>
2026-06-17 12:00:19 +0530

    Create 71-SimplifyPath

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/71-SimplifyPath/version/cpp/71-SimplifyPath_20260617_120015.cpp
@@ -0,0 +1,36 @@
+// Last updated: 17/06/2026, 12:00:15
+// i have used AI here ... and i have invited one of my friend to solve this problem
+1class Solution {
+2public:
+3    string simplifyPath(string path) {
+4        vector<string> st;
+5        
+6        stringstream ss(path);
+7        
+8        string store;
+9        
+10        
+11        while(getline(ss,store,'/')){
+12            if(store == "" || store == "."){
+13                continue;
+14            }
+15            else if(store == ".."){
+16                if(!st.empty()){
+17                    st.pop_back();
+18                }
+19            }
+20            else{
+21                st.push_back(store);
+22            }
+23            
+24            
+25           
+26        }
+27        string res = "";
+28        
+29         for(const string& ch : st){
+30                res += "/" + ch;
+31            }
+32        return res.empty()?"/" : res;
+33    }
+34};
