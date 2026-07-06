54188f1f2f30118ccf1c8ea1694742a7b519fd82
Amrit Raj <amrit.raj.cg@gmail.com>
2026-07-06 22:56:26 +0530

    Create 1-TwoSum

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/1-TwoSum/version/cpp/1-TwoSum_20260706_225623.cpp
@@ -0,0 +1,16 @@
+// Last updated: 06/07/2026, 22:56:23
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        
+5        unordered_map<int,int>mp;
+6        for(int i = 0; i < nums.size() ; i++){
+7            int diff = target - nums[i];
+8            if(mp.find(diff) != mp.end()){
+9                return {mp[diff],i};
+10            }
+11            mp[nums[i]] = i;
+12        }
+13        return {};
+14    }
+15};
