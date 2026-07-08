ce5b5cfa62305be3d3e125b36f3fba839fb93843
Amrit Raj <amrit.raj.cg@gmail.com>
2026-07-08 14:20:50 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(int i = 0 ; i < details.size() ; i++){
+            if(details[i].size() >= 14 && stoi(details[i].substr(11,2)) > 60){
+                count++;
+            }
+        }
+        return count;
+    }
+};
