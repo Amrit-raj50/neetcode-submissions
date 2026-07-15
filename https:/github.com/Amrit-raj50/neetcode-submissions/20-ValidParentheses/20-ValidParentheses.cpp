b5fe300520b636c67d6c3573574d297c827aced3
Amrit Raj <amrit.raj.cg@gmail.com>
2026-07-15 21:44:05 +0530

    Create 20-ValidParentheses

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/20-ValidParentheses/20-ValidParentheses.cpp
@@ -0,0 +1,27 @@
+// Last updated: 15/07/2026, 21:44:04
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> sk;
+
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                sk.push(s[i]);
+            }
+            else{
+                if(sk.empty())
+                return false;
+
+                if((sk.top() == '(' && s[i] == ')')||
+                  (sk.top() == '{' && s[i] == '}')||
+                  (sk.top() == '[' && s[i] == ']')){
+                    sk.pop();
+                  }
+                  else{
+                    return false;
+                  }
+            }
+        }
+        return sk.empty();
+    }
+};
