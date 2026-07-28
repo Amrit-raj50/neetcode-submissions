28ac5c4e657767dd7064b289888927caca44b89b
Amrit Raj <amrit.raj.cg@gmail.com>
2026-07-28 17:58:17 +0530

    Create 1086-DivisorGame

---
+++ https:/github.com/Amrit-raj50/neetcode-submissions/1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,12 @@
+// Last updated: 28/07/2026, 17:58:17
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if(n % 2 == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
